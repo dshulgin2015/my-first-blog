0b7c89c6ea981f12d3382643212eca906cc6fea0
dshulgin2015 <unetby2016@yandex.ru>
2016-11-16 16:48:14 +0300

    Modified Dog commit css header done

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all().order_by('published_date')
+    posts = Post.objects.all().order_by('-published_date')
     print(posts)
     return render(request, 'blog/post_list.html', {'posts': posts})
